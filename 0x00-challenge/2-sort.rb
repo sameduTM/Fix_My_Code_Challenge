3c9521ffa64d1b3565fc9bf82b3e4e031f4cc91c
Ken Wekesa <wanyamak884@gmail.com>
2023-12-23 22:43:47 +0300

    ruby fix code

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -6,12 +6,13 @@
 
 result = []
 ARGV.each do |arg|
+
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
     i = 0
@@ -20,7 +21,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
